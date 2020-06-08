6ac3485694797c4da69d7cbdf8a7c59cc7a188c5
jkzasori <jotafu@live.com>
2020-06-08 16:46:41 -0500

    Funcionalidad para validar numeros OK

--- pruebas.js
+++ pruebas.js
@@ -1,6 +1,6 @@
 const m = require('.');
 console.log(m.suma(1,2))
 console.log(m.resta(1,2))
-console.log(m.resta(11,24))
+console.log(m.resta(true,24))
 console.log(m.multiplicacion(1,24))
-console.log(m.suma(1,2))
+console.log(m.suma(1,-1))
