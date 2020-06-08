da8995f11754bb21a37748fa09a49db55095d535
jkzasori <jotafu@live.com>
2020-06-08 15:34:55 -0500

    Primera version del modulo de matematicas basicas OK

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    /**
+     * Suma de dos números
+     * @example
+     * n1 = 1, n2 = 3 => resultado 4
+     * @param {*} n1 Número uno de la Suma
+     * @param {*} n2 Número dos de la Suma
+     */
+    suma: function (n1, n2) {
+        return n1 + n2;
+    },
+    /**
+     * Resta de dos números
+     * @example
+     * n1 = 1, n2 = 3 => resultado -2
+     * @param {*} n1 Número uno de la Resta
+     * @param {*} n2 Número dos de la Resta
+     */
+    resta: function (n1, n2) {
+        return n1 - n2;
+    },
+        /**
+     * Multiplicación de dos números
+     * @example
+     * n1 = 1, n2 = 3 => resultado 3
+     * @param {*} n1 Número uno de la Multiplicación
+     * @param {*} n2 Número dos de la Multiplicación
+     */
+    multiplicacion: function (n1, n2) {
+        return n1 * n2;
+    },
+        /**
+     * División de dos números
+     * @example
+     * n1 = 1, n2 = 3 => resultado 0,333
+     * @param {*} n1 Número uno de la División
+     * @param {*} n2 Número dos de la División
+     */
+    division: function (n1, n2) {
+        return n1 / n2;
+    }
+}
